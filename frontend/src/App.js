c07267bdc38d0fb620666b71547f71c29debbb6c
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-07-01 23:09:26 +0530

    Feat:Frontend added react routing || Form to add damaged roads - motivation: added UI for user to easily upload photos and videos of damages roads - changes: added form for adding the images and videos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,23 @@ import "./App.css";
 import Home from "./Components/Home";
 import IssueForm from "./Components/form"
 import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+       <BrowserRouter>
+     
       
-      <IssueForm />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/form" element={<IssueForm/>} />
+      </Routes>
+      </BrowserRouter>
      
     </div>
   );
